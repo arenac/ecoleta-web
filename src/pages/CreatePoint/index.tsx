f5dcc3ab260be2c26b8326c1e977b8191a888cf8
Nilo Neregato <neregato.nilo@gmail.com>
2020-06-06 23:47:10 +0200

    form setup

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,11 +1,128 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './CreatePoint.css';
+import logo from '../../assets/logo.svg';
 
 const CreatePoint: React.FC = () => {
   return (
-    <div>
-      <h1>Create Point</h1>
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Return to home
+        </Link>
+      </header>
+
+      <form action="">
+        <h1>Register Collect Point</h1>
+
+        <fieldset>
+          <legend>
+            <h2>Address</h2>
+            <span>Select the address</span>
+          </legend>
+
+          <Map center={[59.189180, 17.619574]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[59.189180, 17.619574]} />
+          </Map>
+
+          <div className="field">
+            <label htmlFor="name">Entity name</label>
+            <input 
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input 
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input 
+                type="text"
+                name="whatsapp"
+                id="whatsapp"
+              />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Address</h2>
+            <span>Select an address</span>
+          </legend>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">State</label>
+              <select name="uf" id="uf">
+                <option value="0">Select a State</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="city">City</label>
+              <select name="city" id="city">
+                <option value="0">Select a city</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+        
+        <fieldset>
+          <legend>
+            <h2>Collect Items</h2>
+            <span>Select one or more items</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="Oil"/>
+              <span>Kitchen Oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="Oil"/>
+              <span>Kitchen Oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="Oil"/>
+              <span>Kitchen Oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="Oil"/>
+              <span>Kitchen Oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="Oil"/>
+              <span>Kitchen Oil</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oil.svg" alt="Oil"/>
+              <span>Kitchen Oil</span>
+            </li>
+          </ul>
+        </fieldset>
+
+        <button type="submit">
+          Registe a collect point
+        </button>
+      </form>
     </div>
   )
 }
